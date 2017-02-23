a911fa01460a76b2358e61eb07ee64b8984ed4f8
Tang Ning <tangning111@gmail.com>
2017-02-23 12:22:16 +0800

    Add react-virtualized example

--- examples/entry.js
+++ examples/entry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { VContainer } from '../src';
+import { render, findDOMNode } from 'react-dom';
+import { List } from 'react-virtualized';
 
 const sample_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere massa non suscipit suscipit. Nullam nisl massa, pretium vel ligula a, auctor mollis velit. Vivamus volutpat eu nulla ac pellentesque. Aliquam leo tortor, congue vitae mattis nec, sagittis non elit. Maecenas suscipit non elit eu ultrices. Maecenas sodales nec orci sit amet tristique. Nulla porttitor, magna a dignissim vestibulum, eros libero sodales ante, ultrices euismod justo augue pellentesque quam. Integer vitae nunc nec nisi ullamcorper dignissim. Nulla sodales sem id arcu interdum, consequat imperdiet odio vehicula. Sed et vulputate diam, ut vestibulum urna. Suspendisse commodo venenatis purus nec ultrices. Praesent velit neque, aliquet non urna viverra, ornare egestas diam. Nulla sapien tortor, rutrum vel tempus a, semper vel metus.';
 
@@ -13,9 +13,7 @@ class Image extends Component {
 class Text extends Component {
 	render() {
 		return (
-			<span>
-				{ this.props.text }
-			</span>
+			<div>{ this.props.text }</div>
 		);
 	}
 }
@@ -57,27 +55,75 @@ const items = [...Array(1000).keys()].map(function(item, index) {
 	}
 });
 
+let list_ref, last_total_height;
+const refs = {}, heights = {};
+const getRowHeight = ({ index }) => {
+	const node = refs[index];
+
+	if (node) {
+		const dom = findDOMNode(node);
+		const height = dom.getBoundingClientRect().height;
+
+		if (height !== heights[index]) {
+			heights[index] = height;
+		}
+
+		return height;
+	}
+
+	return 40;
+}
+
+setInterval(() => {
+	if (list_ref) {
+		const total_height = Object.values(heights).reduce((v, acc) => acc + v, 0);
+		// recomputeRowHeights is quite expensive
+		// thus we only call it when total height has changed
+		if (total_height !== last_total_height) {
+			list_ref.recomputeRowHeights();
+		}
+	}
+}, 30);
+
 render(
-	<VContainer
-		style={ {
-			height: '100%',
-			width: '100%',
-			position: 'fixed',
-			top: 0,
-			left: 0,
-			overflow: 'auto'
-		} }
-		items={ items }
-		initialHeight={ 50 }
-		maxRows={ 50 }
-		renderItem={ function(item) {
+	<List
+		ref={ ref => list_ref = ref }
+		height={ window.innerHeight }
+		width={ window.innerWidth }
+		rowCount={ items.length }
+		rowHeight={ getRowHeight }
+		rowRenderer ={ function({ key, index, isScrolling, isVisible, style }) {
+			const item = items[index];
+
+			// HACK
+			// CellRenderer will set width to 100% if it's writable
+			// but our items have dynamic width, thus we don't want them to be stretched
+			if (style) {
+				Object.defineProperty(style, 'width', {
+					writable: false,
+					value: 'auto'
+				});
+			}
+
 			switch (item.type) {
 				case 'image':
-					return <Image src={ item.src } />;
+					return (
+						<div style={ style } key={ key }>
+							<Image ref={ ref => refs[index] = ref } src={ item.src } />
+						</div>
+					);
 				case 'text':
-					return <Text text={ item.text } />;
+					return (
+						<div style={ style } key={ key }>
+							<Text ref={ ref => refs[index] = ref } text={ item.text } />
+						</div>
+					);
 				case 'iframe':
-					return <Iframe />;
+					return (
+						<div style={ style } key={ key }>
+							<Iframe ref={ ref => refs[index] = ref } />
+						</div>
+					);
 				default:
 					return null;
 			}
